1c942e218b8d5a219e4c68c2689f9900c57564d8
relja456 <93168552+relja456@users.noreply.github.com>
2022-10-16 20:51:22 +0200

    menu to fit content

---
+++ script.js
@@ -0,0 +1,28 @@
+let menuitem1 = document.getElementById('menu1') 
+let menuitem2 = document.getElementById('menu2') 
+let menuitem3 = document.getElementById('menu3') 
+let menuitem4 = document.getElementById('menu4') 
+let menuitem5 = document.getElementById('menu5') 
+
+let burger = document.getElementById('toggle1')
+
+console.log(7272772)
+menuitem1.onclick = function() {
+    burger.click()
+}
+
+menuitem2.onclick = function() {
+    burger.click()
+}
+
+menuitem3.onclick = function() {
+    burger.click()
+}
+
+menuitem4.onclick = function() {
+    burger.click()
+}
+
+menuitem5.onclick = function() {
+    burger.click()
+}
