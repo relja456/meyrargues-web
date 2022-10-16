5369a384fb50f80a896103187da9d083feff5760
relja456 <93168552+relja456@users.noreply.github.com>
2022-10-17 00:15:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let menuitem0 = document.getElementById('menu0') 
 let menuitem1 = document.getElementById('menu1') 
 let menuitem2 = document.getElementById('menu2') 
 let menuitem3 = document.getElementById('menu3') 
@@ -7,6 +8,10 @@ let menuitem5 = document.getElementById('menu5')
 let burger = document.getElementById('toggle1')
 
 console.log(7272772)
+menuitem0.onclick = function() {
+    burger.click()
+}
+
 menuitem1.onclick = function() {
     burger.click()
 }
